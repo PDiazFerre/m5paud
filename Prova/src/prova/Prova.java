17f23c2e9fc883554e6a8d3f61c77bb312267663
paudc <paudc@MATEBOOK-PADICA>
2023-01-09 19:03:42 +0100

    afegit un for

--- Prova/src/prova/Prova.java
+++ Prova/src/prova/Prova.java
@@ -14,7 +14,13 @@ public class Prova {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int malala = 100;
+        
+        for (int i = 1; i < malala; i++){
+        
+        System.out.println(i);
+        }
     }
     
 }
